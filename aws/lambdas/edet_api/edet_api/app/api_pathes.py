04cd163f430fde836a1a3f8fbd4dbe81791d7495
michaelgr22 <michael.graml22@outlook.de>
2021-08-05 11:15:26 +0200

    created api for edet

---
+++ aws/lambdas/edet_api/edet_api/app/api_pathes.py
@@ -0,0 +1,125 @@
+def mcknews(db, event):
+    number_of_news = event['queryStringParameters']['number']
+    tablename = 'mck.news'
+
+    sql = """SELECT news_headline, news_date, news_category, news_imagelink, news_content FROM {}
+    WHERE news_category = 'Fussball' OR news_category = 'Allgemein'
+    ORDER BY news_date desc
+    LIMIT %s;""".format(tablename)
+
+    values = (number_of_news)
+    result = db.execute_sql(sql, values=values)
+
+    return list(map(lambda row: {'news_headline': row[0], 'news_date': row[1], 'news_category': row[2], 'news_imagelink': row[3], 'news_content': row[4]}, result))
+
+
+def players(db, event):
+    teamname = event['queryStringParameters']['teamname']
+    teamclass = event['queryStringParameters']['teamclass']
+    teamseason = event['queryStringParameters']['teamseason']
+    players_tablename = 'teams.players'
+    teams_tablename = 'teams.all_teams'
+
+    sql = """SELECT player_firstname, player_lastname, player_birthday, player_deployments, player_goals, player_position, player_imagelink
+    FROM {0}
+    JOIN {1} ON {0}.player_team_id={1}.team_id
+    WHERE team_name = %s AND team_class = %s AND team_season = %s;""".format(players_tablename, teams_tablename)
+
+    values = (teamname, teamclass, teamseason)
+    result = db.execute_sql(sql, values=values)
+
+    return list(map(lambda row: {'player_firstname': row[0], 'player_lastname': row[1], 'player_birthday': row[2], 'player_deployments': row[3],
+                                 'player_goals': row[4], 'player_position': row[5], 'player_imagelink': row[6]}, result))
+
+
+def teammatches(db, event):
+    teamname = event['queryStringParameters']['teamname']
+    teamclass = event['queryStringParameters']['teamclass']
+    teamseason = event['queryStringParameters']['teamseason']
+
+    matches_tablename = 'matches.all_matches'
+    teams_tablename = 'teams.all_teams'
+    leagues_tablename = 'leagues.all_leagues'
+
+    sql = """SELECT match_date_time, home.team_showname as match_home_team_showname, away.team_showname as match_away_team_showname, 
+match_link, match_home_goals, match_away_goals, match_cancelled, leagues.league_showname as match_league_showname
+FROM {0}
+JOIN {1} home ON {0}.match_home_team_id=home.team_id
+JOIN {1} away ON {0}.match_away_team_id=away.team_id
+JOIN {2} leagues ON {0}.match_league_id=leagues.league_id
+WHERE (home.team_name = %s AND home.team_class = %s AND home.team_season = %s) 
+OR (away.team_name = %s AND away.team_class = %s AND away.team_season = %s)
+ORDER BY match_date_time asc;""".format(matches_tablename, teams_tablename, leagues_tablename)
+
+    values = (teamname, teamclass, teamseason, teamname, teamclass, teamseason)
+    result = db.execute_sql(sql, values=values)
+
+    return list(map(lambda row: {'match_date_time': row[0], 'match_home_team_showname': row[1], 'match_away_team_showname': row[2], 'match_link': row[3],
+                                 'match_home_goals': row[4], 'match_away_goals': row[5], 'match_cancelled': row[6], 'match_league_showname': row[7]}, result))
+
+
+def standings(db, event):
+    teamname = event['queryStringParameters']['teamname']
+    teamclass = event['queryStringParameters']['teamclass']
+    teamseason = event['queryStringParameters']['teamseason']
+
+    standings_tablename = 'leagues.standings'
+    teams_tablename = 'teams.all_teams'
+
+    sql = """SELECT standings_position, teams.team_showname as standings_team_showname, standings_games, standings_wins, 
+    standings_draws, standings_loses, standings_goals, standings_countered_goals, standings_points 
+FROM {0}
+JOIN {1} team_leagues ON {0}.standings_league_id=team_leagues.team_main_league_id
+JOIN {1} teams ON {0}.standings_team_id=teams.team_id
+WHERE (team_leagues.team_name = %s AND team_leagues.team_class = %s AND team_leagues.team_season = %s)
+ORDER BY standings_position asc;""".format(standings_tablename, teams_tablename)
+
+    values = (teamname, teamclass, teamseason)
+    result = db.execute_sql(sql, values=values)
+
+    return list(map(lambda row: {'standings_position': row[0], 'standings_team_showname': row[1], 'standings_games': row[2], 'standings_wins': row[3],
+                                 'standings_draws': row[4], 'standings_loses': row[5], 'standings_goals': row[6], 'standings_countered_goals': row[7],
+                                 'standings_points': row[8]}, result))
+
+
+def mainleague(db, event):
+    teamname = event['queryStringParameters']['teamname']
+    teamclass = event['queryStringParameters']['teamclass']
+    teamseason = event['queryStringParameters']['teamseason']
+
+    leagues_tablename = 'leagues.all_leagues'
+    teams_tablename = 'teams.all_teams'
+
+    sql = """SELECT league_showname, league_name, league_season 
+FROM {0}
+JOIN {1} AS teams ON {0}.league_id=teams.team_main_league_id
+WHERE teams.team_name = %s AND teams.team_class = %s AND teams.team_season = %s;""".format(leagues_tablename, teams_tablename)
+
+    values = (teamname, teamclass, teamseason)
+    result = db.execute_sql(sql, values=values)
+
+    return list(map(lambda row: {'league_showname': row[0], 'league_name': row[1], 'league_season': row[2]}, result))
+
+
+def leaguematches(db, event):
+    teamname = event['queryStringParameters']['teamname']
+    teamclass = event['queryStringParameters']['teamclass']
+    teamseason = event['queryStringParameters']['teamseason']
+
+    matches_tablename = 'matches.all_matches'
+    teams_tablename = 'teams.all_teams'
+
+    sql = """SELECT match_date_time, home.team_showname as match_home_team_showname, away.team_showname as match_away_team_showname, 
+match_link, match_home_goals, match_away_goals, match_cancelled
+FROM {0}
+JOIN {1} teams ON {0}.match_league_id=teams.team_main_league_id
+JOIN {1} home ON {0}.match_home_team_id=home.team_id
+JOIN {1} away ON {0}.match_away_team_id=away.team_id
+WHERE teams.team_name = %s AND teams.team_class = %s AND teams.team_season = %s
+ORDER BY match_date_time asc;""".format(matches_tablename, teams_tablename)
+
+    values = (teamname, teamclass, teamseason)
+    result = db.execute_sql(sql, values=values)
+
+    return list(map(lambda row: {'match_date_time': row[0], 'match_home_team_showname': row[1], 'match_away_team_showname': row[2], 'match_link': row[3],
+                                 'match_home_goals': row[4], 'match_away_goals': row[5], 'match_cancelled': row[6]}, result))
---
+++ aws/lambdas/edet_api/edet_api/app/app.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+from aws_dbconnector_client import AwsDBConnectorClient
+from . import api_pathes
+
+database = os.environ.get('DB')
+secret_name = os.environ.get('SECRET')
+secret_region = os.environ.get('REGION')
+aws_dbconnector_client = AwsDBConnectorClient(
+    secret_name, secret_region, database, 'postgres')
+db = aws_dbconnector_client.get_db()
+
+
+def get_response(event):
+    print(event['path'])
+    if event['path'] == '/mcknews':
+        return api_pathes.mcknews(db, event)
+    elif event['path'] == '/players':
+        return api_pathes.players(db, event)
+    elif event['path'] == '/teammatches':
+        return api_pathes.teammatches(db, event)
+    elif event['path'] == '/leaguematches':
+        return api_pathes.leaguematches(db, event)
+    elif event['path'] == '/standings':
+        return api_pathes.standings(db, event)
+    elif event['path'] == '/mainleague':
+        return api_pathes.mainleague(db, event)
+
+
+def lambda_handler(event, context):
+    statusCode = 200
+    response = {}
+
+    try:
+        response = get_response(event)
+        if not response:
+            statusCode = 401
+    except:
+        statusCode = 400
+
+    responseObject = {}
+    responseObject['statusCode'] = statusCode
+    responseObject['headers'] = {}
+    responseObject['headers']['Content-Type'] = 'application/json'
+    responseObject['body'] = json.dumps(response, default=str)
+
+    return responseObject
