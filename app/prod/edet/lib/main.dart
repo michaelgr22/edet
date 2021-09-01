2563fda235df7de4969e688e89c5616620b64b1d
michaelgr22 <michael.graml22@outlook.de>
2021-09-01 13:30:10 +0200

    add firebase crashlytics

--- app/prod/edet/lib/main.dart
+++ app/prod/edet/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   runApp(App());
 }
